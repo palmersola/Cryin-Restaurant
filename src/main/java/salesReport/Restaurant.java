f295a9cd58ad9d07c95194613a4b73e14699fa3a
palmersola <palmersola@yahoo.com>
2023-06-06 15:22:24 -0500

    added Restaurant

---
+++ src/main/java/salesReport/Restaurant.java
@@ -0,0 +1,57 @@
+package salesReport;
+
+import inventoryManagment.Ingredient;
+import inventoryManagment.IngredientList;
+import orderProcessing.Order;
+import orderProcessing.OrderService;
+import tableManagement.Table;
+import tableManagement.TableList;
+import userLogin.LoginSystem;
+
+public class Restaurant {
+    private String name;
+    private LoginSystem login;
+    private IngredientList ingredients;
+    private TableList tables;
+    private OrderService orders;
+
+    public Restaurant(String name) {
+        this.name = name;
+        this.login = new LoginSystem();
+        this.ingredients = new IngredientList();
+        this.tables = new TableList();
+        this.orders = new OrderService();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LoginSystem getLogin() {
+        return login;
+    }
+
+    public IngredientList getIngredients() {
+        return ingredients;
+    }
+
+    public TableList getTables() {
+        return tables;
+    }
+
+    public OrderService getOrders() {
+        return orders;
+    }
+
+    public void addIngredient(Ingredient item) {
+        this.ingredients.addIngredient(item);
+    }
+
+    public void addTable(Table item) {
+        this.tables.addTable(item);
+    }
+
+    public void addOrder(Order item) {
+        this.orders.addOrder(item);
+    }
+}
