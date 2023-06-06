483b13915b2f1b0e7c3ed07dda77e6e40ee080d6
caramel <AlexanderForward149@gmail.com>
2023-06-06 12:16:56 -0400

    Alexander's Main added

---
+++ src/main/java/orderProcessing/OrderMain.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderMain {
+    public static void main(String[] args) {
+        OrderService system = new OrderService();
+
+        // Creating orders
+        Order order1 = new Order(1, Map.of("Chicken Nuggies", 2, "Fires", 1), 150.0);
+        Order order2 = new Order(2, Map.of("Milkshake", 3), 75.0);
+
+        // Adding orders to the system
+        system.addOrder(order1);
+        system.addOrder(order2);
+
+        // Updating order status
+        system.updateOrderStatus(1, OrderStatus.PREPARING);
+        system.updateOrderStatus(2, OrderStatus.READY);
+
+        // Displaying order status
+        system.displayOrderStatus(1);
+        system.displayOrderStatus(2);
+        system.displayOrderStatus(3);
+    }
+}
